7d9b16ed8e1b55f5f963717550200c585d25e57b
ranjith <rajenranjith004@gmail.com>
2017-10-21 13:05:06 +0530

    third commit

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-users',
@@ -6,11 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-  constructor() {
+  sub;
+  userId;
+  data;
+  constructor(private route: ActivatedRoute,
+    private router: Router, private http: Http) {
      
    }
 
   ngOnInit() {
+    this.sub = this.route
+      .queryParams
+      .subscribe(params => {
+        // Defaults to 0 if no query param provided.
+        this.userId = +params['userId'] || 0;
+        console.log(this.userId)
+          this.http.get("https://jsonplaceholder.typicode.com/todos?userId="+this.userId)
+            .subscribe((data)=> {
+                setTimeout(()=> {
+                    this.data = data.json();
+                    console.log(this.data)
+                }, 1000);
+            });
+        //https://jsonplaceholder.typicode.com/todos?userId=1
+      });
   }
   
 }
